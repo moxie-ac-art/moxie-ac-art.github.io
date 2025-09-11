e49ff566a45bbde72d42be751d055c290dc24ad9
Elie Carlos <ecc226@nau.edu>
2025-09-10 20:15:54 -0700

    Fixing gh-pages deployment

---
+++ art-site/next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true
+    },
+    assetprefix: isProd ? '/' : '',
+    basePath: isProd ? '' : '',
+    output: 'export'
+};
+
+export default nextConfig;
