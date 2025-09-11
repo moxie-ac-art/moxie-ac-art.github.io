769ea2bd095502352778757f9e38be23b5584c77
Elie Carlos <ecc226@nau.edu>
2025-09-11 12:40:57 -0700

    Fix 404 errors

--- art-site/next.config.mjs
+++ art-site/next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     reactStrictMode: true,
     images: {
         unoptimized: true
     },
-    assetprefix: isProd ? '/' : '',
-    basePath: isProd ? '' : '',
     output: 'export'
 };
 
