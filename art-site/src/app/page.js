859773bc36bfabfb4a471e20bf92ab45e9d8667d
Elie Carlos <ecc226@nau.edu>
2025-09-11 18:58:11 -0700

    Added logo and additional pages

--- art-site/src/app/page.js
+++ art-site/src/app/page.js
@@ -2,12 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          Moxie Aradia-Carlos Art
-        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           Hi! We're Shay & Elie Aradia-Carlos. Shay makes beautiful ceramic pieces, and Elie makes rugs, prints, & stickers.
           Please bear with us as we tidy up our website. We're happy to have you here!
@@ -23,7 +18,5 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-    </div>
   );
 }
