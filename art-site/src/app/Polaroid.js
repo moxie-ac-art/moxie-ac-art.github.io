f25cbbb472272233a5cd9f094581c4946059d5a2
Elie Carlos <ecc226@nau.edu>
2025-09-12 20:34:36 -0700

    Fix layout and populate about page

--- art-site/src/app/Polaroid.js
+++ art-site/src/app/Polaroid.js
@@ -1,17 +1,17 @@
 "use client";
 import Image from "next/image";
 
-export default function Polaroid({src, alt, text}) {
+export default function Polaroid({src, alt, text, width=110, height=110}) {
     return (
         <div>
-            <svg width="160" height="190">
+            <svg width="150" height="190">
                 <rect width="150" height="190" fill="white" />
                 </svg>
             <Image 
                 src={src}
                 alt={alt}
-                width={110}
-                height={110}
+                width={width}
+                height={height}
                 className="absolute bottom-18 left-15"
             />
             <p className="absolute bottom-12 left-16">{text}</p>
--- art-site/src/app/about/page.js
+++ art-site/src/app/about/page.js
@@ -1,5 +1,8 @@
 import { Beth_Ellen } from "next/font/google";
 
+import Polaroid from "../Polaroid";
+import Note from "../Note";
+
 const bethEllen = Beth_Ellen({
   variable: "--font-beth-ellen",
   weight:'400',
@@ -7,24 +10,49 @@ const bethEllen = Beth_Ellen({
 });
 
 export default async function Page() {
-    const style = "flex flex-col items-center justify-between bg-[#FFC8C8] rounded-xl p-10 " + bethEllen.className;
+    const style = "flex flex-col items-center justify-between bg-[#FFC8C8] rounded-xl p-10 w-[600px] text-[14px] " + bethEllen.className;
     return (
         <main className={style}>
-            <div>
-                <p>
-                    Our story started when we met as roommates at NAU up in Flagstaff. 
-                    We’ve been together for 5 years and got married in January this year!
-                </p>
+            <div className="flex flex-row items-center justify-between">
+                <Note 
+                    text="Our story started when we met as roommates at NAU up in Flagstaff. 
+                    We’ve been together for 5 years and got married in January this year!"
+                    color="#D9D9D9"
+                />
+                <div className="relative flex items-center p-10 rotate-7">
+                    <Polaroid 
+                        src="./Shay and Elie Wedding.jpg"
+                        alt="Photo and Shay and Elie at their wedding"
+                        text="Shay & Elie"
+                    />
+                </div>
             </div>
-            <div>
-                <p>
-                    Shay (she/they) started making ceramics in high school and college. She has continued making wonderful pieces and is the Secretary of the Arizona Clay Association.
-                </p>
+            <div className="flex flex-row items-center justify-between">
+                <div className="relative flex items-center p-10 -rotate-7">
+                    <Polaroid 
+                        src="./Shay Ceramics.jpg"
+                        alt="Shay throwing a ceramic plate"
+                        text="Shay"
+                        width={100}
+                    />
+                </div>
+                <Note 
+                    text="Shay (she/they) started making ceramics in high school and college. She has continued making wonderful pieces and is the Secretary of the Arizona Clay Association."
+                    color="#D9D9D9"
+                />
             </div>
-            <div>
-                <p>
-                    Elie (they/she) started punch needling & rug tufting during lockdown, and has been drawing since they were little. They have done art markets at DreamWorks Animation.
-                </p>
+            <div className="flex flex-row items-center justify-between">
+                <Note 
+                    text="Elie (they/she) started punch needling & rug tufting during lockdown, and has been drawing since they were little. They have done art markets at DreamWorks Animation."
+                    color="#D9D9D9"
+                />
+                <div className="relative flex items-center p-10 rotate-7">
+                    <Polaroid 
+                        src="./Elie Rug.jpg"
+                        alt="Elie holding a Shrek rug next to a statue of Shrek"
+                        text="Elie"
+                    />
+                </div>
             </div>
         </main>
     );
